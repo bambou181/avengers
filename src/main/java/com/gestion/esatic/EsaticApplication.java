0c6a9e84f80686d958742156289fa82e596f04d1
bambou181 <patrickmouroufie56@gmail.com>
2023-12-06 13:33:21 +0000

    starting

---
+++ src/main/java/com/gestion/esatic/EsaticApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.esatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/esatic/EsaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.esatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
